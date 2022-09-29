deed1fa5861c36fba92d7bbb67994efc9bd9d511
Spitfier720 <velomn7@gmail.com>
2022-09-29 00:50:46 -0400

    I think I was supposed to optimize this a bit.

---
+++ 2015/Day 10 - Elves Look, Elves Say Part 2.py
@@ -0,0 +1,24 @@
+#Find the length of a sequence of numbers after applying a look-and-say sequence 50 times.
+
+digits = input()
+
+for x in range(50):
+    #Create a new string for the next iteration, a placeholder for the last digit, and a count to group consecuitive same digits
+    newDigits, pastDigit, count = "", "", 0
+
+    #Iterate through the digits and group them.
+    for digit in digits:
+        if(pastDigit != digit and pastDigit):
+            newDigits += str(count) + pastDigit
+            count = 1
+        
+        else:
+            count += 1
+        
+        pastDigit = digit
+    
+    #Account for the last group which will not be caught by the for loop.
+    newDigits += str(count) + pastDigit
+    digits = newDigits
+
+print(len(newDigits))
