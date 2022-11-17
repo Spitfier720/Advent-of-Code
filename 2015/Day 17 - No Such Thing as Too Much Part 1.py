4055339facd5d330952e800024f33801848ec732
Spitfier720 <56697279+Spitfier720@users.noreply.github.com>
2022-11-17 00:19:50 -0500

    I hope to figure out itertools someday.

--- 2015/Day 17 - No Such Thing as Too Much Part 1.py
+++ 2015/Day 17 - No Such Thing as Too Much Part 1.py
@@ -5,7 +5,9 @@ import itertools, sys
 containers = [int(x.strip()) for x in sys.stdin.readlines()]
 comboCount = 0
 
+#1-indexed since the combination must hold at least one element.
 for i in range(1, len(containers) + 1):
+    #Using itertools.combinations due to the fact that it's faster than recursively finding the algorithm.
     for x in itertools.combinations(containers, i):
         if(sum(x) == 150):
             comboCount += 1
