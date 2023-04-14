21e320633ecccc9316968d2cc770f6d53437ffda
Spitfier720 <56697279+Spitfier720@users.noreply.github.com>
2023-04-14 16:24:07 -0400

    These two parts are almost completely identical.

---
+++ 2016/Day 6 - Signals and Noise Part 1.py
@@ -0,0 +1,19 @@
+#Decrypting a signal using repetition coding.
+
+import sys
+
+message = ""
+signals = list(x for x in sys.stdin.readlines())
+
+#Assuming that each signal is the same length.
+#We also subtract 1 so we don't add a newline character that comes with every string.
+for x in range(len(signals[0]) - 1):
+    letterCount = {}
+
+    for y in signals:
+        letterCount[y[x]] = letterCount.setdefault(y[x], 0) + 1
+    
+    #A nice one-liner that gets the "max" key through the maximum value.
+    message += max(letterCount, key = letterCount.get)
+
+print(message)
