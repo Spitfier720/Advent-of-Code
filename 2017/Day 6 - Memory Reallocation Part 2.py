8c4e5351ae16426676019ec771d2990d2c0126cd
Spitfier720 <56697279+Spitfier720@users.noreply.github.com>
2023-05-04 14:58:12 -0400

    I could have implemented this using classes.
    Why are the 2 parts so similar though

---
+++ 2017/Day 6 - Memory Reallocation Part 2.py
@@ -0,0 +1,39 @@
+#Following a reallocation process until we get a self-loop.
+
+curConfig = tuple(map(int, input().split()))
+length = len(curConfig)
+configs = {}
+steps = 0
+
+while(curConfig not in configs):
+    configs[curConfig] = steps
+
+    maxElement = 0
+    index = 0
+
+    for x in range(length):
+        if(curConfig[x] > maxElement):
+            maxElement = curConfig[x]
+            index = x
+    
+    newConfig = []
+
+    for x in range(length):
+        offset = x - index
+
+        if(offset <= 0):
+            offset += length
+        
+        #Since we keep adding 1 to the next element until we run out, we can shorten this into a formula.
+        blocksToAdd = int((((maxElement - offset) / length) + 1) // 1)
+        
+        if(x == index):
+            newConfig.append(blocksToAdd)
+
+        else:
+            newConfig.append(curConfig[x] + blocksToAdd)
+    
+    steps += 1
+    curConfig = tuple(newConfig)
+
+print(steps - configs[curConfig])
