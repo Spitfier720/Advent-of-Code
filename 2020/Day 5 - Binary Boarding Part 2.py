ff5300655773dfefc4738b98793938bb3c0d3051
Spitfier720 <56697279+Spitfier720@users.noreply.github.com>
2023-07-25 17:20:12 -0400

    I never seem to code binary search correctly.
    It's always some off-by-one error, I'll need to look into that.

---
+++ 2020/Day 5 - Binary Boarding Part 2.py
@@ -0,0 +1,56 @@
+#Using binary search to single out a seat in a grid.
+
+import sys
+
+#Holds the ranges in which the seat could be in, and with enough partitioning, will narrow the range to a single number.
+class RowsBinary:
+    def __init__(self, length):
+        self.__left = 0
+        self.__right = length
+        self.__middle = (self.__right // 2)
+    
+    #The right half of the range will be dropped, leaving only the left half to be considered.
+    def partitionLeft(self):
+        self.__right = self.__middle
+        self.__middle = (self.__left + self.__right) // 2
+    
+    #The left half of the range will be dropped, leaving only the right half to be considered.
+    def partitionRight(self):
+        self.__left = self.__middle + 1
+        self.__middle = (self.__left + self.__right) // 2
+    
+    def getResult(self):
+        return self.__left if self.__left == self.__right else None
+
+#Finds the right seat using the RowsBinary class and returns its String ID.
+def findSeat(stringInput):
+    row = RowsBinary(127)
+    
+    #Getting the row.
+    for x in stringInput[:7]:
+        match x:
+            case "F":
+                row.partitionLeft()
+            
+            case "B":
+                row.partitionRight()
+    
+    col = RowsBinary(7)
+
+    #Getting the column.
+    for x in stringInput[7:]:
+        match x:
+            case "L":
+                col.partitionLeft()
+
+            case "R":
+                col.partitionRight()
+    
+    return (row.getResult() * 8) + col.getResult()
+
+seatIDs = set(findSeat(x) for x in sys.stdin.readlines())
+
+for x in seatIDs:
+    if(x + 1 not in seatIDs and x + 2 in seatIDs):
+        print(x + 1)
+        break
