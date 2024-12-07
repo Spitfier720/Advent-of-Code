4190ecf0ce91210c340a23ef082eae097d948af5
Michael Ge <56697279+Spitfier720@users.noreply.github.com>
2024-12-07 01:14:11 -0500

    This day is so free what the hell

---
+++ 2024/Day 7 - Bridge Repair Part 1.py
@@ -0,0 +1,21 @@
+import sys
+
+def canFormResult(result, operands):
+    if(len(operands) == 1):
+        return operands[0] == result
+
+    return canFormResult(result, [operands[0] + operands[1]] + operands[2:]) or \
+           canFormResult(result, [operands[0] * operands[1]] + operands[2:])
+
+sum = 0
+
+for line in sys.stdin.readlines():
+    line = line.split(":")
+
+    result = int(line[0])
+    operands = list(map(int, line[1].strip().split(" ")))
+
+    if(canFormResult(result, operands)):
+        sum += result
+
+print(sum)
