629b53bd9010c694b6015771e63bf2bad8ec79e1
Michael Ge <56697279+Spitfier720@users.noreply.github.com>
2024-12-08 01:01:48 -0500

    As optimized as it needs to be

---
+++ 2024/Day 8 - Resident Collinearity Part 1.py
@@ -0,0 +1,40 @@
+import sys
+
+def getAntinodes(antennaLoc, length, width):
+    antinodeLoc = set()
+
+    for i in range(len(antennaLoc)):
+        for j in range(i + 1, len(antennaLoc)):
+            difference = (antennaLoc[j][0] - antennaLoc[i][0], antennaLoc[j][1] - antennaLoc[i][1])
+
+            if(inBounds((antennaLoc[i][0] - difference[0], antennaLoc[i][1] - difference[1]), length, width)):
+                antinodeLoc.add((antennaLoc[i][0] - difference[0], antennaLoc[i][1] - difference[1]))
+            
+            if(inBounds((antennaLoc[j][0] + difference[0], antennaLoc[j][1] + difference[1]), length, width)):
+                antinodeLoc.add((antennaLoc[j][0] + difference[0], antennaLoc[j][1] + difference[1]))
+    
+    return antinodeLoc
+
+def inBounds(loc, length, width):
+    return 0 <= loc[0] < length and 0 <= loc[1] < width
+
+length, width = 0, 0
+
+antennae = {}
+
+for line in sys.stdin.readlines():
+    line = line.strip()
+
+    for i in range(len(line)):
+        if(line[i] != "."):
+            antennae.setdefault(line[i], []).append((length, i))
+
+    width = len(line)
+    length += 1
+
+unqLoc = set()
+
+for key in antennae:
+    unqLoc.update(getAntinodes(antennae[key], length, width))
+
+print(len(unqLoc))
